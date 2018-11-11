9f67ce5d029878b3df15597df8ff3e35d0829d16
k911 <konradobal@gmail.com>
2018-11-11 23:50:05 +0100

    build(docker): Provide configuration for testing

--- tests/run-server-tests.sh
+++ tests/run-server-tests.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+cd "$( dirname "${BASH_SOURCE[0]}" )";
+
 EXIT_CODE=0
 
-for f in ./tests/Server/*.sh; do
+for f in ./Server/*.sh; do
     echo "[Test] $f";
     if [[ "$COVERAGE" == "1" ]]; then
         APP_ENV=cov SWOOLE_ALLOW_XDEBUG=1 bash "$f" -H || EXIT_CODE=1;
